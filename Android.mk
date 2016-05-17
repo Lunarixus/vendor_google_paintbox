d492b5aed27fd4dc9821e2953400ef6c27342292
Todd Poynor <toddpoynor@google.com>
2016-05-16 18:49:18 -0700

    libmockeasel: Add TCP/IP-based mock Easel communication
    TCP/IP mock implementation of Easel communication, plus some TCP/IP-
    specific add-ons for server hostname/IP and service port number in new
    classes EaselCommClientNet and EaselCommServerNet.
    
    Change-Id: If4d411ca85b513e429102d5da219b2d3ba975e7a

--- Android.mk
+++ Android.mk
@@ -1,5 +1,18 @@
 # Copyright 2016 The Android Open Source Project
 
 ifeq ($(TARGET_USES_PAINTBOX), true)
-  include $(call all-makefiles-under, $(call my-dir))
+
+LOCAL_PATH:= $(call my-dir)
+PB_AOSP_INCLUDES := $(TOP)/hardware/google_paintbox/include
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmockeasel
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(PB_AOSP_INCLUDES)
+LOCAL_CFLAGS += -Wall -Werror -UNDEBUG
+LOCAL_SRC_FILES := \
+	EaselCommNet.cpp
+include $(BUILD_STATIC_LIBRARY)
+
+include $(call all-makefiles-under, $(call my-dir))
+
 endif
