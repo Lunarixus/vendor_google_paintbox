a4e1424329765c8f37641e6edd0b90cac1142681
Todd Poynor <toddpoynor@google.com>
2016-08-08 05:56:24 -0700

    ezlsh: move to new utils/ dir
    Anticipating further utils in the future, and moving things out of the
    top-level vendor/goog_paintbox dir.
    
    Change-Id: I525d913ef78dec25fd7bf8dfacdb94831d019fc7

---
+++ utils/Android.mk
@@ -0,0 +1,14 @@
+# Copyright 2016 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+PB_AOSP_INCLUDES := $(TOP)/hardware/google/paintbox/include
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ezlsh
+LOCAL_CFLAGS += -Wall -Werror -UNDEBUG -DMOCKEASEL
+LOCAL_SRC_FILES := ezlsh.cpp
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(PB_AOSP_INCLUDES)
+LOCAL_STATIC_LIBRARIES := libmockeasel
+include $(BUILD_EXECUTABLE)
+
