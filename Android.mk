8143b1ee47243d7c2b685c5cda5476ff2076b2dd
Todd Poynor <toddpoynor@google.com>
2016-08-08 14:30:13 -0700

    libmockeasel: move files to libeasel/
    Move easelcomm mock-specific files, and files that will be shared
    between mock and real comm libs, to libeasel/ subdir.
    
    Change-Id: Ib6f188c4aea1773a92fcdf856333899679498b02

--- Android.mk
+++ Android.mk
@@ -1,30 +1,5 @@
 # Copyright 2016 The Android Open Source Project
 
 ifeq ($(TARGET_USES_PAINTBOX), true)
-
-LOCAL_PATH:= $(call my-dir)
-PB_AOSP_INCLUDES := $(TOP)/hardware/google/paintbox/include
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libmockeasel
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(PB_AOSP_INCLUDES)
-LOCAL_CPPFLAGS += -Wall -Werror -UNDEBUG -DMOCKEASEL
-LOCAL_SRC_FILES := \
-	EaselCommNet.cpp \
-	EaselControlClient.cpp \
-	EaselControlServer.cpp
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := mockeaselcomm_test
-LOCAL_MODULE_TAGS := tests
-LOCAL_CFLAGS += -UNDEBUG -DMOCKEASEL
-LOCAL_SRC_FILES := mockeaselcomm_test.cpp
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_C_INCLUDES := $(PB_AOSP_INCLUDES)
-LOCAL_STATIC_LIBRARIES := libmockeasel liblog
-include $(BUILD_NATIVE_TEST)
-
-include $(call all-makefiles-under, $(call my-dir))
-
+  include $(call all-makefiles-under,$(call my-dir))
 endif
--- libeasel/Android.mk
+++ libeasel/Android.mk
@@ -11,3 +11,23 @@ LOCAL_CPPFLAGS += -Wall -Werror -UNDEBUG
 LOCAL_SRC_FILES := \
 	EaselComm.cpp
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmockeasel
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(PB_AOSP_INCLUDES)
+LOCAL_CPPFLAGS += -Wall -Werror -UNDEBUG -DMOCKEASEL
+LOCAL_SRC_FILES := \
+        EaselCommNet.cpp \
+        EaselControlClient.cpp \
+        EaselControlServer.cpp
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mockeaselcomm_test
+LOCAL_MODULE_TAGS := tests
+LOCAL_CFLAGS += -UNDEBUG -DMOCKEASEL
+LOCAL_SRC_FILES := mockeaselcomm_test.cpp
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_C_INCLUDES := $(PB_AOSP_INCLUDES)
+LOCAL_STATIC_LIBRARIES := libmockeasel liblog
+include $(BUILD_NATIVE_TEST)
