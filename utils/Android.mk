420e447b26d37c8fd26540ecca17cbe8804c6e55
Trevor Bunker <trevorbunker@google.com>
2016-11-17 13:24:00 -0800

    ezlsh: add OWNER and PATH tags
    Change-Id: If02ee013e2dd49ab6b6ad83672e4a9363c22667e

--- utils/Android.mk
+++ utils/Android.mk
@@ -4,6 +4,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ezlsh
+LOCAL_MODULE_OWNER := google
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/bin
 LOCAL_CFLAGS += -Wall -Werror -UNDEBUG
 LOCAL_SRC_FILES := ezlsh.cpp
 LOCAL_FORCE_STATIC_EXECUTABLE := true
