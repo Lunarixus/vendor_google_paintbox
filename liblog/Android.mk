0bbe876451faba62ed5b06f5e83c1843c942a1bf
Chenjie Luo <cjluo@google.com>
2017-03-07 15:44:00 -0800

    Linking libeasellog dynamically to libunwind.so and liblzma.so.
    To save space on easel.
    
    Test: test on device.
    Change-Id: Ib8a24200d06faaebc6f59bf2e87d22215390ef76

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -6,7 +6,9 @@ LOCAL_SRC_FILES := \
 	log.cpp \
 	log_stub.cpp
 
-LOCAL_STATIC_LIBRARIES := libeasel libdebuggerd libbacktrace libbase libunwind liblzma
+LOCAL_STATIC_LIBRARIES := libeasel libdebuggerd libbacktrace libbase
+
+LOCAL_SHARED_LIBRARIES := libunwind liblzma
 
 LOCAL_CFLAGS += -Wall -Wextra -Werror
 
