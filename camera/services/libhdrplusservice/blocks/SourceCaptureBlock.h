d5c6a3a9f12934a35c3e37b2818045d23b1bda5e
Chien-Yu Chen <cychen@google.com>
2017-05-02 18:29:06 -0700

    libhdrplusservice: Prevent Easel frame drops
    Capture service needs to have 2 requests at all time to prevent
    frame drops. We need to make sure that we have 3 outstanding
    requests so when 1 capture is done, capture service still has 2.
    
    Also create a thread to notify AP about Easel timestamp so the
    request thread won't be blocked by sending the message to AP.
    
    Test: GCA
    Bug: 37544642
    Change-Id: I6a4106c9d7258283140d17de5b129b532224e4fc

--- camera/services/libhdrplusservice/blocks/SourceCaptureBlock.h
+++ camera/services/libhdrplusservice/blocks/SourceCaptureBlock.h
@@ -9,6 +9,7 @@
 namespace pbcamera {
 
 class DequeueRequestThread;
+class TimestampNotificationThread;
 
 /*
  * SourceCaptureBlock is a pipeline block that captures frames from MIPI (or the client for testing
@@ -106,6 +107,9 @@ private:
     // Remove any staled pending output result.
     void removeTimedoutPendingOutputResult();
 
+    // Request a capture to prevent possible frame drops.
+    void requestCaptureToPreventFrameDrop();
+
     // Messenger for transferring the DMA buffer.
     std::shared_ptr<MessengerToHdrPlusClient> mMessengerToClient;
 
@@ -125,6 +129,9 @@ private:
 
     // A DequeueRequestThread to dequeue completed buffers from capture service.
     std::unique_ptr<DequeueRequestThread> mDequeueRequestThread;
+
+    // A thread to notify AP about Easel timestmap.
+    std::unique_ptr<TimestampNotificationThread> mTimestampNotificationThread;
 
     // Whether to pause capturing from MIPI to work around b/34854987.
     std::mutex mPauseLock;
@@ -150,6 +157,13 @@ public:
 private:
     const static int64_t kNsPerMs = 1000000;
 
+    // Capture service needs at least 2 requests at all time to prevent frame drops. We need to
+    // have at least 3 pending requests so when capture service is done with 1, it still has 2.
+    const static uint32_t kMinNumPendingRequests = 3;
+
+    // Check the number of pending requests and request more if needed to prevent frame drops.
+    void checkNumberPendingRequests();
+
     SourceCaptureBlock* mParent;
 
     // Protecting mPendingCaptureRequests and mExiting.
@@ -160,6 +174,33 @@ private:
     std::unique_ptr<std::thread> mDequeueRequestThread;
     std::condition_variable mEventCondition;
     bool mFirstCaptureDone;
+};
+
+// TimestampNotificationThread creates a thread to send Easel timestamps to AP.
+class TimestampNotificationThread {
+public:
+    TimestampNotificationThread(std::shared_ptr<MessengerToHdrPlusClient> messengerToClient);
+    virtual ~TimestampNotificationThread();
+
+    // Notify a new Easel timestamp asynchronously.
+    void notifyNewEaselTimestampNs(int64_t easelTimestampNs);
+
+    // Thread loop that sends Easel timestamps to AP.
+    void threadLoop();
+
+    // Signal the thread to exit.
+    void signalExit();
+
+private:
+    std::unique_ptr<std::thread> mThread;
+    std::shared_ptr<MessengerToHdrPlusClient> mMessengerToClient;
+
+    std::mutex mEventLock;
+    std::condition_variable mEventCondition;
+
+    // The following variables must be protected by mEventLock.
+    bool mExiting; // If requested to exit.
+    std::deque<int64_t> mEaselTimestamps; // A queue of Easel timestamps to send to AP.
 };
 
 } // namespace pbcamera
