b12686ecf612d1e261a12f433f9c0840563dcae3
Chenjie Luo <cjluo@google.com>
2017-08-04 15:00:50 -0700

    Update MallocAHardwareBufferEaselLoopback
    We find a bug when sending a large (>2KB) ion buffer from easel to
    AP. If AP side receiving buffer is malloc based, AP will hang and
    reboot (b/64398844). Now we modify the test server to receive and
    loopback malloc buffer instead of ion buffer to temporarily
    workaround the bug.
    
    Test: easelcomm2_test
    Bug: 64398844
    Change-Id: I3fa50999047068e8185883ceca4548eadf058399

--- test_infra/tests/libeaselcomm/EaselComm2TestServer.cpp
+++ test_infra/tests/libeaselcomm/EaselComm2TestServer.cpp
@@ -77,7 +77,7 @@ void handleStructMessage(const EaselComm2::Message& message2) {
 }
 
 // Handles DMA ion buffer and echo same buffer back.
-void handleBufferMessage(const EaselComm2::Message& message2) {
+void handleIonBufferMessage(const EaselComm2::Message& message2) {
   ImxDeviceBufferHandle buffer;
   size_t size = message2.getPayload().size;
   CHECK_EQ(
@@ -93,10 +93,25 @@ void handleBufferMessage(const EaselComm2::Message& message2) {
   EaselComm2::HardwareBuffer hardwareBuffer(fd, size, 0);
   CHECK_EQ(server->receivePayload(message2, &hardwareBuffer), NO_ERROR);
   // Replies the same message.
-  CHECK_EQ(server->send(kBufferChannel, {hardwareBuffer}), NO_ERROR);
+  CHECK_EQ(server->send(kIonBufferChannel, {hardwareBuffer}), NO_ERROR);
 
   CHECK_EQ(ImxDeleteDeviceBuffer(buffer), IMX_SUCCESS);
 }
+
+// Handles DMA malloc buffer and echo same buffer back.
+void handleMallocBufferMessage(const EaselComm2::Message& message2) {
+  ImxDeviceBufferHandle buffer;
+  size_t size = message2.getPayload().size;
+  void* vaddr = malloc(size);
+  CHECK(vaddr != nullptr);
+
+  EaselComm2::HardwareBuffer hardwareBuffer(vaddr, size, 0);
+  CHECK_EQ(server->receivePayload(message2, &hardwareBuffer), NO_ERROR);
+  // Replies the same message.
+  CHECK_EQ(server->send(kMallocBufferChannel, {hardwareBuffer}), NO_ERROR);
+  free(vaddr);
+}
+
 }  // namespace
 
 int main() {
@@ -109,7 +124,8 @@ int main() {
   while (true) {
     CHECK_EQ(server->open(EASEL_SERVICE_TEST), android::NO_ERROR);
 
-    server->registerHandler(kBufferChannel, handleBufferMessage);
+    server->registerHandler(kIonBufferChannel, handleIonBufferMessage);
+    server->registerHandler(kMallocBufferChannel, handleMallocBufferMessage);
     server->registerHandler(kProtoChannel, handleProtoMessage);
     server->registerHandler(kStructChannel, handleStructMessage);
     server->registerHandler(kStringChannel, handleStringMessage);
